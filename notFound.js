718aaf4daa3716ecefb32806465bd9214f03b137
Charl <105985808+CharlRae@users.noreply.github.com>
2025-01-27 21:37:01 +0000

    Add files via upload

---
+++ notFound.js
@@ -0,0 +1,3 @@
+// There's nothing worth looking at here either, unless you're just interested!
+let currentUrl = window.location.pathname;
+document.getElementById('errorMessage').textContent = `${currentUrl} doesn't exist!`;
